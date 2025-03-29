9b0282cb32ee0d3161e98ceaa0c3cf5ee4c52b3e
kaveenhyacinth <alwiskaveen@gmail.com>
2025-03-29 06:36:52 +0530

    feat: improve error handling in department and role services with specific exceptions

--- api/src/modules/role/role.service.ts
+++ api/src/modules/role/role.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role } from '../../database/core/role.entity';
 import { Repository } from 'typeorm';
@@ -6,36 +10,24 @@ import { ROLE } from '../../enums/role.enum';
 
 @Injectable()
 export class RoleService {
-  private logger = new Logger('RoleService');
-
   constructor(
     @InjectRepository(Role)
     private readonly roleRepo: Repository<Role>,
   ) {}
 
   async findById(roleId: string) {
-    if (!roleId) throw new Error('roleId not found');
+    if (!roleId) throw new BadRequestException('roleId is required');
 
-    try {
-      const role = await this.roleRepo.findOneBy({ id: roleId });
-      if (!role) throw new Error('Role not found');
-      return role;
-    } catch (err) {
-      this.logger.error(err);
-      throw new Error('Something went wrong when finding the role', err);
-    }
+    const role = await this.roleRepo.findOneBy({ id: roleId });
+    if (!role) throw new NotFoundException('Role not found');
+    return role;
   }
 
   async findByName(roleName: ROLE) {
-    if (!roleName) throw new Error('roleName not found');
+    if (!roleName) throw new BadRequestException('roleName is required');
 
-    try {
-      const role = await this.roleRepo.findOneBy({ roleName });
-      if (!role) throw new Error('Role not found');
-      return role;
-    } catch (err) {
-      this.logger.error(err);
-      throw new Error('Something went wrong when finding the role');
-    }
+    const role = await this.roleRepo.findOneBy({ roleName });
+    if (!role) throw new NotFoundException('Role not found');
+    return role;
   }
 }
